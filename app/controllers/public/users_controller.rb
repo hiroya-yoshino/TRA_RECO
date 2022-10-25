97b1b7dfb54eb3db733a145add431e244ef42654
Hiroya Yoshino <108524504+hiroya->
2022-10-25 12:00:34 +0000

    未登録ユーザアクセス制限の更新

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,5 @@
 class Public::UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
