d577e35c8dc05c364b2d4a4956e071bdd0fc83a6
Hiroya Yoshino <108524504+hiroya->
2022-10-25 10:56:00 +0000

    ゲストユーザ処理の追加

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,5 +1,6 @@
 class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :guest_check!
 
   def create
     @post = Post.find(params[:post_id])
@@ -12,5 +13,11 @@ class Public::CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:detail, :post_id, :user_id)
+  end
+
+  def guest_check
+    if current_user.email == 'guest@example.com'
+      redirect_to root_path, notice: "このページを見るにはユーザ登録が必要です。"
+    end
   end
 end
