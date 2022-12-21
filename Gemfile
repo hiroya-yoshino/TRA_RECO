7cab0de9a7933030c02fb28d9ae5857fb51d5975
EC2 Default User <ec2-user@ip-172-31-37-210.ap-northeast-1.compute.internal>
2022-12-21 12:55:29 +0000

    [Add]geocoderとpost_detailに追記

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ end
 gem "net-smtp"
 gem "net-pop"
 gem "net-imap"
+
+gem "geocoder"
--- app/models/post_detail.rb
+++ app/models/post_detail.rb
@@ -1,5 +1,7 @@
 class PostDetail < ApplicationRecord
   belongs_to :post
+  geocoded_by :address
+  after_validation :geocode
 
   has_one_attached :image
 
