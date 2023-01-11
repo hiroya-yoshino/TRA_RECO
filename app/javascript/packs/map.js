a7b118666bfad7fe58cb8d8e75eccb408509c6bd
EC2 Default User <ec2-user@ip-172-31-37-210.ap-northeast-1.compute.internal>
2023-01-11 10:09:23 +0000

    [Add]地図ページwebpaker経由でのjavascriptの読み込み

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,6 +1,6 @@
 function initMap(){
     // 地図の位置情報(緯度・経度)を取得
-    let mapPosition = {lat:  post_detail.latitude.presence || 0  , lng:  post_detail.longitude.presence || 0  };
+    let mapPosition = {lat: post_detail.latitude.presence || 0 , lng: post_detail.longitude.presence || 0 };
     let map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
       center: mapPosition
